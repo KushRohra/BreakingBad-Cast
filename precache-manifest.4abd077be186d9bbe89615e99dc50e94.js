22120165ceb27bf932e1f5742174e175787d72e9
KushRohra <iec2017024@iiita.ac.in>
2020-07-25 00:42:15 +0530

    Updates

---
+++ precache-manifest.4abd077be186d9bbe89615e99dc50e94.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "08b93317c6ad8a44d2f2ce2fcc808634",
+    "url": "/BreakingBad-Cast/index.html"
+  },
+  {
+    "revision": "62599a4e55d3605e3316",
+    "url": "/BreakingBad-Cast/static/css/main.04b7df6d.chunk.css"
+  },
+  {
+    "revision": "1ffd321d95fb218ff183",
+    "url": "/BreakingBad-Cast/static/js/2.005fcb9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/BreakingBad-Cast/static/js/2.005fcb9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62599a4e55d3605e3316",
+    "url": "/BreakingBad-Cast/static/js/main.80d3b63f.chunk.js"
+  },
+  {
+    "revision": "d0045ac7832206b35d7e",
+    "url": "/BreakingBad-Cast/static/js/runtime-main.e844cbcd.js"
+  },
+  {
+    "revision": "1be2d3a0dc2064fe215cebb23799d503",
+    "url": "/BreakingBad-Cast/static/media/bg.1be2d3a0.jpg"
+  },
+  {
+    "revision": "3795f66d4a869a7faeb4b425446daf33",
+    "url": "/BreakingBad-Cast/static/media/logo.3795f66d.png"
+  },
+  {
+    "revision": "235b9b3ed13f64542cb0792e6ae7b5c4",
+    "url": "/BreakingBad-Cast/static/media/spinner.235b9b3e.gif"
+  }
+]);
